c9827d5d258df052d568567d8c6f2154acbaa652
worthant <b_dvorkin@niuitmo.ru>
2024-11-25 15:35:19 +0300

    :hammer: feat(lab2/lifo): Working lifo implementation

--- lab2/lab2.srcs/sources_1/new/top.sv
+++ lab2/lab2.srcs/sources_1/new/top.sv
@@ -2,7 +2,7 @@
 
 typedef struct {
     bit [3:0] stack_mem [0:7];    // Stack memory with 8 elements
-    bit [2:0] sp;                 // Stack pointer (0 to 7)
+    bit [3:0] sp;                 // Stack pointer (0 to 7)
     bit full;
     bit empty;
 } lifo_t;
@@ -64,6 +64,7 @@ module top(
         .rst(BTNC),
         .push(push_edge),
         .pop(pop_edge),
+        .switch_reset(SW[15]),
         .data_in(SW[3:0]),
         .data_out(LED[3:0]),
         .lifo_state(lifo_state)
@@ -108,7 +109,7 @@ module display (
 
         current_digit = lifo_state.stack_mem[digit_select];
         case (current_digit)
-            4'h0: segment_pattern = 7'b0000010;
+            4'h0: segment_pattern = 7'b0000001;
             4'h1: segment_pattern = 7'b1001111;
             4'h2: segment_pattern = 7'b0010010;
             4'h3: segment_pattern = 7'b0000110;
@@ -127,11 +128,11 @@ module display (
             default: segment_pattern = 7'b1111111;
         endcase
 
-        cathodes[6:0] = segment_pattern[6:0];
+        cathodes[7:1] = segment_pattern[6:0];
         if (digit_select == lifo_state.sp) begin
-            cathodes[7] = 1'b0; // DP on
+            cathodes[0] = 1'b0; // DP on
         end else begin
-            cathodes[7] = 1'b1; // DP off
+            cathodes[0] = 1'b1; // DP off
         end
     end
 endmodule
@@ -168,6 +169,7 @@ module lifo(
     input logic rst,
     input logic push,
     input logic pop,
+    input logic switch_reset,
     input logic [3:0] data_in,      // SW 0-3
     output logic [3:0] data_out,    // Top of the stack
     output lifo_t lifo_state        // Current state of the stack
@@ -185,7 +187,7 @@ module lifo(
     end
 
     always_ff @(posedge clk or posedge rst) begin
-        if (rst) begin
+        if (rst && !switch_reset) begin
             // Reset stack
             stack.sp <= 0;
             stack.full <= 0;
@@ -198,7 +200,7 @@ module lifo(
             // Push operation
             if (push && !stack.full) begin
                 stack.stack_mem[stack.sp] <= data_in;
-                if (stack.sp != 7) stack.sp <= stack.sp + 1;
+                stack.sp <= stack.sp + 1;
                 stack.empty <= 0;
                 if (stack.sp == 7) stack.full <= 1;
                 data_out <= data_in;
